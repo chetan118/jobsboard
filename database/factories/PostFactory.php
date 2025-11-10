cd3b54fcd01f6a3816994eacd4a8c43ae5621d2f
Chetan Sakpal <chetan.sakpal@gmail.com>
2025-11-10 18:23:52 +0000

    Factories for comments and posts
    Signed-off-by: Chetan Sakpal <chetan.sakpal@gmail.com>

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'body' => fake()->paragraphs(asText: true),
+            'user_id' => User::factory(),
         ];
     }
 }
